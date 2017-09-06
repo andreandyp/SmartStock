afc7b9587b324b64ae0c2fbb7f3f049a11bc8368
André Michel Andy Pozos <andreandyp@outlook.com>
2017-09-06 10:30:43 -0500

    Sección de reportes mejorada

---
+++ app/src/main/java/com/smartstock/ParticularFragment.java
@@ -0,0 +1,26 @@
+package com.smartstock;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ParticularFragment extends Fragment {
+
+
+    public ParticularFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_particular, container, false);
+    }
+
+}
